fca139bb2828096f0f40a4c35039f2077d5fd38c
George Jabley <techwokx@gmail.com>
2026-06-08 01:17:44 +0000

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,2 +1,23 @@
-import sys, os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+# modules/__init__.py
+# This file makes the modules directory a Python package
+
+from modules.company_research import research_company, CompanyResearchResult
+from modules.lead_scoring import score_from_research
+from modules.technology_detector import detect_technologies
+from modules.crm import save_research_to_crm, log_activity
+from modules.database import get_session, ResearchHistory, CRMCompany, CRMActivity
+from modules.theme import THEME_CSS
+
+__all__ = [
+    'research_company',
+    'CompanyResearchResult',
+    'score_from_research',
+    'detect_technologies',
+    'save_research_to_crm',
+    'log_activity',
+    'get_session',
+    'ResearchHistory',
+    'CRMCompany',
+    'CRMActivity',
+    'THEME_CSS'
+]
